278af82b9337acac186e9248f4aff36438abc006
Idrissa <idrisabkar2018@gmail.com>
2023-11-13 17:22:02 +0300

    app layout config

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1 className="text-4xl">HELLO ATAKAN</h1>
+      <h1>heşş</h1>
     </div>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+function Navbar() {
+  return (
+    <nav>
+      <h1>MixMaster</h1>
+    </nav>
+  )
+}
+
+function NavItems() {
+  return (
+    <ul>
+      <li>Home</li>
+      <li>About</li>
+      <li>NewsLetter</li>
+    </ul>
+  )
+}
+
+export default Navbar
